a3d0d41c13c132df790a4dab9871bb82d9e3a524
V1ctorNunes <victor.moreira@sptech.school>
2023-10-13 22:19:27 -0300

    Versão final das Telas.

---
+++ Tela Sobre_Nos/script.js
@@ -0,0 +1,9 @@
+function gerenciarMenu() {
+  menu.style.display = 'block'
+
+  if (menu.style.display == 'none') {
+      menu.style.display = 'block' 
+  if (menu.style.display == 'block'){
+      menu.style.display = 'none'
+  }}
+}
